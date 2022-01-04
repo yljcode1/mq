9dee20acfb60b638d04ab37be89169ed4dc57838
yljcode1 <aaa2826455837@gmail.com>
2022-01-04 20:57:50 +0800

    顺序消息添加注释

--- example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
@@ -29,19 +29,33 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+/**
+ * 顺序消息
+ */
 public class Producer {
     public static void main(String[] args) throws UnsupportedEncodingException {
         try {
             DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
             producer.start();
 
-            String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+            String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
             for (int i = 0; i < 100; i++) {
                 int orderId = i % 10;
-                Message msg =
-                    new Message("TopicTestjjj", tags[i % tags.length], "KEY" + i,
-                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                Message msg = new Message("TopicTest", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+                /**
+                 *  参数 1：消息对象
+                 *  参数 2：消息队列选择器
+                 *  参数 3：选择队列的业务标识
+                 */
                 SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                    /**
+                     * 消息队列
+                     * @param mqs 队列集合
+                     * @param msg 消息对象
+                     * @param arg 业务标识
+                     * @return 队列
+                     */
                     @Override
                     public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
                         Integer id = (Integer) arg;
